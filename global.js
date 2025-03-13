7476486ad70090c806ccd48048108553dbbeb1ee
Gustavo Tironi <B51083@fgv.edu.br>
2025-03-13 15:46:56 -0300

    Arrumei o link

--- global.js
+++ global.js
@@ -14,9 +14,9 @@ const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
 let pages = [
     {url: "", title: "Home"},
-    {url: "projects/", title: "Projects"},
-    {url: "resume/", title: "Resume"},
-    {url: "contact/", title: "Contact"},
+    {url: "/projects/", title: "Projects"},
+    {url: "/resume/", title: "Resume"},
+    {url: "/contact/", title: "Contact"},
     {url: "https://github.com/gtironi/dataviz_01", title: "GitHub"}
 ];
 
