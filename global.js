0840437365ae449c4f3ce198cb25e910c96c43e3
Gustavo Tironi <B51083@fgv.edu.br>
2025-03-13 15:53:03 -0300

    fix 2

--- global.js
+++ global.js
@@ -13,10 +13,10 @@ function $$ (selector, context = document) {
 const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
 let pages = [
-    {url: "/", title: "Home"},
-    {url: "/projects/", title: "Projects"},
-    {url: "/resume/", title: "Resume"},
-    {url: "/contact/", title: "Contact"},
+    {url: "", title: "Home"},
+    {url: "./projects/", title: "Projects"},
+    {url: "./resume/", title: "Resume"},
+    {url: "./contact/", title: "Contact"},
     {url: "https://github.com/gtironi/dataviz_01", title: "GitHub"}
 ];
 
